9d361668faeeafd602a4a3a94c75a4748e355b78
GiseleNunes <giselenunes@sciosolutions.com.br>
2019-11-12 10:09:23 -0300

    Added config to Storybook

---
+++ storybook-static/precache-manifest.6d10e4e8c61494b3795be50a656ab5f4.js
@@ -0,0 +1,19 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22c3f82624bb119fb0d71120c7188f45",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.74fa205e65b84b45c54d.bundle.js"
+  },
+  {
+    "revision": "3c1fe36161dc01e4288f",
+    "url": "main.css"
+  },
+  {
+    "url": "runtime~main.74fa205e65b84b45c54d.bundle.js"
+  },
+  {
+    "url": "vendors~main.74fa205e65b84b45c54d.bundle.js"
+  }
+]);
